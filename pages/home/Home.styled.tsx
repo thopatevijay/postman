852d70dd09733501806a2779bdb2300192b637c2
thopatevijay <thopatevijay@gmail.com>
2023-08-12 01:33:39 +0530

    project setup : added styled component

---
+++ pages/home/Home.styled.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+import { Button as UiButton } from 'antd';
+
+
+export const Button = styled(UiButton)``;
--- pages/home/Home.tsx
+++ pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Button } from 'antd';
+import * as Styled from './Home.styled';
 
 const Home = () => (
   <div className="App">
     <Button type="primary">Button</Button>
+    <Styled.Button type='primary'>Styled Button</Styled.Button>
   </div>
 );
 
