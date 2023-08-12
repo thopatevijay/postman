6c2e7bbad249ab95204350c58a2882b1928866b6
thopatevijay <thopatevijay@gmail.com>
2023-08-12 21:56:43 +0530

    changes in deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,30 @@
 import { ethers } from "hardhat";
+const fs = require('fs');
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log(`Deploying contracts with the account: ${deployer.address}`);
-  
+
   // Deploying the LetterStore contract
   const LetterStore = await ethers.getContractFactory("LetterStore");
   const letterStore = await LetterStore.connect(deployer).deploy();
-  
-  await letterStore.deployed();
 
+  await letterStore.deployed();
   console.log(`LetterStore deployed to: ${letterStore.address}`);
+
+  let deployedDetails = {
+    CONTRACT_NAME: "LetterStore",
+    CONTRACT_ADDRESS: letterStore.address,
+    DEPLOYER_ADDRESS: deployer.address,
+    ABI: JSON.parse(letterStore.interface.format("json").toString())
+  }
+
+  fs.writeFile("./contract-details.json", JSON.stringify(deployedDetails, null, 2), (err: any) => {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log("The file was saved!");
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
