7423e68f82233ba7f25c94f772d9be721dc2cbe0
thopatevijay <thopatevijay@gmail.com>
2023-08-12 23:34:57 +0530

    deplyed contract MODE

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,11 @@ const config: HardhatUserConfig = {
   defaultNetwork: 'ganache',
   solidity: "0.8.17",
   networks: {
+    mode: {
+      url: 'https://sepolia.mode.network/',
+      chainId: 919,
+      accounts: [process.env.PRIVATE_KEY]
+    },
     ganache: {
       url: 'http://localhost:8545',
       accounts: [process.env.PRIVATE_KEY]
