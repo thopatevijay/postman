cf6a3596f769abf14a469c2fe441670881f8e554
thopatevijay <thopatevijay@gmail.com>
2024-07-17 12:54:41 +0530

    connecting wallet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { init, Web3OnboardProvider } from "@web3-onboard/react";
+import { onboardConfig } from "../utils/connectWallet";
+
+const wen3Onboard = init({
+  connect: {
+    autoConnectAllPreviousWallet: true,
+  },
+  ...onboardConfig,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Web3OnboardProvider web3Onboard={wen3Onboard}>
+      <Component {...pageProps} />
+    </Web3OnboardProvider>
+  );
 }
